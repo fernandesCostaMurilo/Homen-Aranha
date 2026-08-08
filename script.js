e4ae3bb7a080749db535aaf324e2c568de35d11b
fernandesCostaMurilo <murilofernandescosta08@gmail.com>
2026-08-08 12:51:20 -0300

    Homen-aranha
    Um site simples feito por IA apenas para aprender a usar o github pages

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", function () {
+
+    alert(
+        "🕷️ Peter Parker é o Homem-Aranha!\n\n" +
+        "Ele possui força, agilidade, reflexos incríveis " +
+        "e a habilidade de escalar paredes."
+    );
+
+});
